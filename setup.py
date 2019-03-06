56a34bc09105b341ecd60257293c14ade5effbd2
Daniele Varrazzo <piro@gambitresearch.com>
2019-03-06 17:08:47 +0000

    Releasing 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="gambit-bromine",
-    version="0.2",
+    version="0.2.1",
     author="Daniele Varrazzo",
     author_email="piro@gambitresearch.com",
     description="Pythonic web testing",
