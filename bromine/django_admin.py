3f68df1352c3ab76b73c0811e9f19b5bf936a37f
Daniele Varrazzo <piro@gambitresearch.com>
2019-03-08 21:57:28 +0000

    Added mixins to interact with select2 objects and Django Admin site

---
+++ bromine/django_admin.py
@@ -0,0 +1,89 @@
+from .select2 import Select2
+
+
+class DjangoAdmin:
+    """A mixin to add django-admin utilities to a browser.
+    """
+
+    def click_save(self):
+        return self(xpath="//input[@name='_save']").click()
+
+    def click_save_cont(self):
+        return self(xpath="//input[@name='_continue']").click()
+
+    def click_save_add(self):
+        return self(xpath="//input[@name='_addanother']").click()
+
+    def click_input(self, value):
+        return self(
+            xpath="//input[@value=%s]" % self._quote_xpath(value)
+        ).click()
+
+    @property
+    def success_msgs(self):
+        self(id="content").wait()
+        return self.find("ul.messagelist li.success")
+
+    @property
+    def warning_msgs(self):
+        self(id="content").wait()
+        return self.find("ul.messagelist li.warning")
+
+    @property
+    def fields(self):
+        return DjangoAdminFields(self)
+
+    @property
+    def widgets(self):
+        return DjangoAdminWidgets(self)
+
+    def get_object_path(self, obj):
+        return "/%s/%s/%s" % (
+            obj._meta.app_label,
+            obj._meta.model_name,
+            obj.pk,
+        )
+
+
+class DjangoAdminFields:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def __getitem__(self, name):
+        try:
+            elem = self.browser("div.field-%s" % name).wait("present")
+        except self.browser.exceptions.NoSuchElementException:
+            raise KeyError("field not found: %s" % name)
+
+        return FieldWrapper(elem, self.browser)
+
+
+class DjangoAdminWidgets:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def __getitem__(self, name):
+        try:
+            return self.browser(id="id_" + name).wait("present")
+        except self.browser.exceptions.NoSuchElementException:
+            raise KeyError("widget not found: %s" % name)
+
+
+class FieldWrapper:
+    def __init__(self, elem, browser):
+        self.elem = elem
+        self.browser = browser
+
+    def __getattr__(self, attr):
+        return getattr(self.elem, attr)
+
+    def __call__(self, *args, **kwargs):
+        return self.elem(*args, **kwargs)
+
+    @property
+    def errors(self):
+        return self.elem.find("ul.errorlist li")
+
+    @property
+    def select2(self):
+        return Select2(self.browser, self.elem)
---
+++ bromine/select2.py
@@ -0,0 +1,56 @@
+class Select2:
+    """A wrapper for an element containing a select2 object.
+    """
+
+    def __init__(self, browser, elem):
+        self.browser = browser
+        self.elem = elem
+
+    @property
+    def text(self):
+        return self.elem(".select2-selection").text
+
+    @property
+    def rendered(self):
+        return self.elem(".select2-selection__rendered")
+
+    def click(self):
+        self.elem(".select2-selection").click()
+        self.browser(".select2-results .loading-results").wait("not present")
+        return self
+
+    def send_keys(self, keys):
+        self.browser("input.select2-search__field").send_keys(keys)
+        return self
+
+    def select(self, text):
+        self.browser(
+            xpath="//li[text()=%s]" % self.browser._quote_xpath(text)
+        ).click()
+        return self
+
+    @property
+    def options(self):
+        return Select2Options(self)
+
+    def wait_option(self, text, condition="present"):
+        self.browser(
+            xpath="//li[text()=]" % self.browser._quote_xpath(text)
+        ).wait(condition)
+        return self
+
+
+class Select2Options:
+    def __init__(self, s2):
+        self.s2 = s2
+
+    def __len__(self):
+        return len(self.s2.browser.find(".select2-results__option"))
+
+    def __iter__(self):
+        return iter(self.s2.browser.find(".select2-results__option"))
+
+    def __call__(self, text):
+        return self.s2.browser(
+            xpath="//li[text()=%s]" % self.s2.browser._quote_xpath(text)
+        )
